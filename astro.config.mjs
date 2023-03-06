46d51e0e83cbf4f808cb3db55e0deae72386e72e
Boston343 <branden.austin1@gmail.com>
2023-03-05 21:40:39 -0500

    this is the extreme basics of sanity integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,22 @@
-import { defineConfig } from 'astro/config';
+import dotenv from "dotenv";
+
+// get .env data for use with process.env
+dotenv.config();
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
-export default defineConfig({});
+import sanity from "astro-sanity";
+
+// https://astro.build/config
+import tailwind from "@astrojs/tailwind";
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [sanity({
+    projectId: "5kells9y",
+    dataset: "production",
+    apiVersion: "2023-03-05",
+    useCdn: true,
+    token: process.env.SANITY_VIEW_TOKEN
+  }), tailwind()]
+});
